43b84dd105939167ce29297fe383cee39f6617e1
DevExpressExampleBot <fakeEmail>
2023-04-13 18:16:29 +0400

    Source auto update [skip ci]

--- VB/LoadDocument/Form1.Designer.vb
+++ VB/LoadDocument/Form1.Designer.vb
@@ -1,109 +1,110 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace LoadDocument
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
-			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
-			Me.btnLoadString = New DevExpress.XtraEditors.SimpleButton()
-			Me.btnLoadStream = New DevExpress.XtraEditors.SimpleButton()
-			Me.btnLoadFile = New DevExpress.XtraEditors.SimpleButton()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.panelControl1.SuspendLayout()
-			Me.SuspendLayout()
-			' 
-			' richEditControl1
-			' 
-			Me.richEditControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.richEditControl1.Location = New System.Drawing.Point(0, 38)
-			Me.richEditControl1.Name = "richEditControl1"
-			Me.richEditControl1.Size = New System.Drawing.Size(644, 372)
-			Me.richEditControl1.TabIndex = 0
-			Me.richEditControl1.Text = "richEditControl1"
-'			Me.richEditControl1.DocumentLoaded += New System.EventHandler(Me.richEditControl1_DocumentLoaded);
-			' 
-			' panelControl1
-			' 
-			Me.panelControl1.Controls.Add(Me.btnLoadString)
-			Me.panelControl1.Controls.Add(Me.btnLoadStream)
-			Me.panelControl1.Controls.Add(Me.btnLoadFile)
-			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
-			Me.panelControl1.Location = New System.Drawing.Point(0, 0)
-			Me.panelControl1.Name = "panelControl1"
-			Me.panelControl1.Size = New System.Drawing.Size(644, 38)
-			Me.panelControl1.TabIndex = 1
-			' 
-			' btnLoadString
-			' 
-			Me.btnLoadString.Location = New System.Drawing.Point(312, 7)
-			Me.btnLoadString.Name = "btnLoadString"
-			Me.btnLoadString.Size = New System.Drawing.Size(149, 23)
-			Me.btnLoadString.TabIndex = 4
-			Me.btnLoadString.Text = "Load Document from String"
-'			Me.btnLoadString.Click += New System.EventHandler(Me.btnLoadString_Click);
-			' 
-			' btnLoadStream
-			' 
-			Me.btnLoadStream.Location = New System.Drawing.Point(157, 7)
-			Me.btnLoadStream.Name = "btnLoadStream"
-			Me.btnLoadStream.Size = New System.Drawing.Size(149, 23)
-			Me.btnLoadStream.TabIndex = 3
-			Me.btnLoadStream.Text = "Load Document from Stream"
-'			Me.btnLoadStream.Click += New System.EventHandler(Me.btnLoadStream_Click);
-			' 
-			' btnLoadFile
-			' 
-			Me.btnLoadFile.Location = New System.Drawing.Point(12, 7)
-			Me.btnLoadFile.Name = "btnLoadFile"
-			Me.btnLoadFile.Size = New System.Drawing.Size(139, 23)
-			Me.btnLoadFile.TabIndex = 0
-			Me.btnLoadFile.Text = "Load Document from File"
-'			Me.btnLoadFile.Click += New System.EventHandler(Me.btnLoadFile_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(644, 410)
-			Me.Controls.Add(Me.richEditControl1)
-			Me.Controls.Add(Me.panelControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.panelControl1.ResumeLayout(False)
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.btnLoadString = New DevExpress.XtraEditors.SimpleButton()
+            Me.btnLoadStream = New DevExpress.XtraEditors.SimpleButton()
+            Me.btnLoadFile = New DevExpress.XtraEditors.SimpleButton()
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panelControl1.SuspendLayout()
+            Me.SuspendLayout()
+            ' 
+            ' richEditControl1
+            ' 
+            Me.richEditControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.richEditControl1.Location = New System.Drawing.Point(0, 38)
+            Me.richEditControl1.Name = "richEditControl1"
+            Me.richEditControl1.Size = New System.Drawing.Size(644, 372)
+            Me.richEditControl1.TabIndex = 0
+            Me.richEditControl1.Text = "richEditControl1"
+            AddHandler Me.richEditControl1.DocumentLoaded, New System.EventHandler(AddressOf Me.richEditControl1_DocumentLoaded)
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Controls.Add(Me.btnLoadString)
+            Me.panelControl1.Controls.Add(Me.btnLoadStream)
+            Me.panelControl1.Controls.Add(Me.btnLoadFile)
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Size = New System.Drawing.Size(644, 38)
+            Me.panelControl1.TabIndex = 1
+            ' 
+            ' btnLoadString
+            ' 
+            Me.btnLoadString.Location = New System.Drawing.Point(312, 7)
+            Me.btnLoadString.Name = "btnLoadString"
+            Me.btnLoadString.Size = New System.Drawing.Size(149, 23)
+            Me.btnLoadString.TabIndex = 4
+            Me.btnLoadString.Text = "Load Document from String"
+            AddHandler Me.btnLoadString.Click, New System.EventHandler(AddressOf Me.btnLoadString_Click)
+            ' 
+            ' btnLoadStream
+            ' 
+            Me.btnLoadStream.Location = New System.Drawing.Point(157, 7)
+            Me.btnLoadStream.Name = "btnLoadStream"
+            Me.btnLoadStream.Size = New System.Drawing.Size(149, 23)
+            Me.btnLoadStream.TabIndex = 3
+            Me.btnLoadStream.Text = "Load Document from Stream"
+            AddHandler Me.btnLoadStream.Click, New System.EventHandler(AddressOf Me.btnLoadStream_Click)
+            ' 
+            ' btnLoadFile
+            ' 
+            Me.btnLoadFile.Location = New System.Drawing.Point(12, 7)
+            Me.btnLoadFile.Name = "btnLoadFile"
+            Me.btnLoadFile.Size = New System.Drawing.Size(139, 23)
+            Me.btnLoadFile.TabIndex = 0
+            Me.btnLoadFile.Text = "Load Document from File"
+            AddHandler Me.btnLoadFile.Click, New System.EventHandler(AddressOf Me.btnLoadFile_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(644, 410)
+            Me.Controls.Add(Me.richEditControl1)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType((Me.panelControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panelControl1.ResumeLayout(False)
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
-		Private panelControl1 As DevExpress.XtraEditors.PanelControl
-		Private WithEvents btnLoadFile As DevExpress.XtraEditors.SimpleButton
-		Private WithEvents btnLoadStream As DevExpress.XtraEditors.SimpleButton
-		Private WithEvents btnLoadString As DevExpress.XtraEditors.SimpleButton
-	End Class
+#End Region
+        Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
+
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+
+        Private btnLoadFile As DevExpress.XtraEditors.SimpleButton
+
+        Private btnLoadStream As DevExpress.XtraEditors.SimpleButton
+
+        Private btnLoadString As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
-
--- VB/LoadDocument/Form1.vb
+++ VB/LoadDocument/Form1.vb
@@ -1,55 +1,52 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports System.IO
 Imports DevExpress.XtraRichEdit
-Imports DevExpress.XtraRichEdit.API.Native
 
 Namespace LoadDocument
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Shared Function GetResourceStream(ByVal resourceName As String) As Stream
-			Return System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
-		End Function
 
-		Private Sub btnLoadStream_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnLoadStream.Click
-			Using stream As Stream = GetResourceStream("LoadDocument.Documents.TextWithImages.rtf")
-				stream.Seek(0, SeekOrigin.Begin)
-				richEditControl1.LoadDocument(stream, DocumentFormat.Rtf)
-				stream.Close()
-			End Using
+    Public Partial Class Form1
+        Inherits Form
 
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub btnLoadFile_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnLoadFile.Click
-			richEditControl1.LoadDocument("Documents\TextWithImages.rtf",DocumentFormat.Rtf)
-		End Sub
+        Private Shared Function GetResourceStream(ByVal resourceName As String) As Stream
+            Return Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
+        End Function
 
-		Private Sub richEditControl1_DocumentLoaded(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.DocumentLoaded
-			Dim documentName As String = richEditControl1.Options.DocumentSaveOptions.CurrentFileName
-			MessageBox.Show("Document file name: " & documentName, "Info")
+        Private Sub btnLoadStream_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Using stream As Stream = GetResourceStream("LoadDocument.Documents.TextWithImages.rtf")
+                stream.Seek(0, SeekOrigin.Begin)
+                richEditControl1.LoadDocument(stream, DocumentFormat.Rtf)
+                stream.Close()
+            End Using
+        End Sub
 
-		End Sub
+        Private Sub btnLoadFile_Click(ByVal sender As Object, ByVal e As EventArgs)
+            richEditControl1.LoadDocument("Documents\TextWithImages.rtf", DocumentFormat.Rtf)
+        End Sub
 
+        Private Sub richEditControl1_DocumentLoaded(ByVal sender As Object, ByVal e As EventArgs)
+            Dim documentName As String = richEditControl1.Options.DocumentSaveOptions.CurrentFileName
+            MessageBox.Show("Document file name: " & documentName, "Info")
+        End Sub
 
-		Private Sub btnLoadString_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnLoadString.Click
-			richEditControl1.RtfText = "{\rtf1\ansi\deff0{\fonttbl{\f0\fnil Times New Roman;}}" & ControlChars.CrLf & "{\colortbl ;\red255\green0\blue0;\red0\green0\blue0;\red255\green192\blue0;\red255\green255\blue0;\red0\green176\blue80;\red0\green176\blue240;" & ControlChars.CrLf & "\red0\green112\blue192;\red112\green48\blue160;\red153\green204\blue255;\red255\green102\blue0;\red0\green204\blue255;\red192\green192\blue192;" & ControlChars.CrLf & "\red255\green153\blue204;\red0\green255\blue0;\red204\green153\blue255;\red51\green51\blue51;\red51\green102\blue255;\red255\green153\blue0;" & ControlChars.CrLf & "\red153\green153\blue153;\red128\green128\blue128;\red255\green204\blue0;\red0\green255\blue255;\red51\green51\blue153;\red204\green255\blue255;}" & ControlChars.CrLf & "\viewkind4\uc1\pard\sl264\slmult1\cf1\lang1033\b\f0\fs44 R\b0 ichard\cf2  \cf3\b O\b0 f\cf2  \cf4\b Y\b0 ork\cf2  \cf5\b G\b0 ave\cf2  \cf6\b B\b0" & ControlChars.CrLf & "attle\cf2  \cf7\b I\b0 n\cf2  \cf8\b V\b0 ain\cf2 .\cf0\fs24\par" & ControlChars.CrLf & "\pard\cf9\fs48 T\cf10 h\cf11 i\cf12 s\cf2  \cf13 i\cf14 s\cf2  \cf15\ul c\cf16 o\cf10 l\cf17 o\cf18 r\cf19 i\cf12 z\cf10 e\cf20 d\cf2\ulnone  \cf20" & ControlChars.CrLf & "t\cf21 e\cf1 x\cf22 t\cf23 .\par\cf2\fs52 This\fs24  \fs32 te\b xt\fs24  \fs96 c\super o\nosupersub n\sub t\nosupersub a\super i\nosupersub n\sub" & ControlChars.CrLf & "s\cf23\nosupersub\fs24  \cf24\ul diffe\cf2 rent \i\fs96 chara\i0 cter f\cf15 or\ulnone mat\cf2 ting in one \b0 paragraph.\cf0\fs24\par" & ControlChars.CrLf & "}"
-		End Sub
-
-
-
-
-
-
-
-
-	End Class
+        Private Sub btnLoadString_Click(ByVal sender As Object, ByVal e As EventArgs)
+            richEditControl1.RtfText = "{\rtf1\ansi\deff0{\fonttbl{\f0\fnil Times New Roman;}}
+{\colortbl ;\red255\green0\blue0;\red0\green0\blue0;\red255\green192\blue0;\red255\green255\blue0;\red0\green176\blue80;\red0\green176\blue240;
+\red0\green112\blue192;\red112\green48\blue160;\red153\green204\blue255;\red255\green102\blue0;\red0\green204\blue255;\red192\green192\blue192;
+\red255\green153\blue204;\red0\green255\blue0;\red204\green153\blue255;\red51\green51\blue51;\red51\green102\blue255;\red255\green153\blue0;
+\red153\green153\blue153;\red128\green128\blue128;\red255\green204\blue0;\red0\green255\blue255;\red51\green51\blue153;\red204\green255\blue255;}
+\viewkind4\uc1\pard\sl264\slmult1\cf1\lang1033\b\f0\fs44 R\b0 ichard\cf2  \cf3\b O\b0 f\cf2  \cf4\b Y\b0 ork\cf2  \cf5\b G\b0 ave\cf2  \cf6\b B\b0
+attle\cf2  \cf7\b I\b0 n\cf2  \cf8\b V\b0 ain\cf2 .\cf0\fs24\par
+\pard\cf9\fs48 T\cf10 h\cf11 i\cf12 s\cf2  \cf13 i\cf14 s\cf2  \cf15\ul c\cf16 o\cf10 l\cf17 o\cf18 r\cf19 i\cf12 z\cf10 e\cf20 d\cf2\ulnone  \cf20
+t\cf21 e\cf1 x\cf22 t\cf23 .\par\cf2\fs52 This\fs24  \fs32 te\b xt\fs24  \fs96 c\super o\nosupersub n\sub t\nosupersub a\super i\nosupersub n\sub
+s\cf23\nosupersub\fs24  \cf24\ul diffe\cf2 rent \i\fs96 chara\i0 cter f\cf15 or\ulnone mat\cf2 ting in one \b0 paragraph.\cf0\fs24\par
+}"
+        End Sub
+    End Class
 End Namespace
